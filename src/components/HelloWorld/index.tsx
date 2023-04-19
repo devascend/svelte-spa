220ab342333d52432095a442c7e44376fb05c0a5
Nicholas Hazel <nicholas.hazel@gmail.com>
2023-04-19 14:50:08 -0700

    test: test

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HelloWorld: React.FC = () => (
     <>
-        <h3>Environmental variables:</h3>
+        <h3>Environmental varisables:</h3>
         <p>
             process.env.NAME: <b>{process.env.NAME}</b>
         </p>
